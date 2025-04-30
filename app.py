ad5f24dd527e755a10b385b7c32f10639397fa5f
marolive <marolive@redhat.com>
2025-04-30 20:45:01 -0300

    Add Swagger

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
+from flasgger import Swagger
 
 app = Flask(__name__)
+
+app.config['SWAGGER'] = {
+    'title': 'My Flask API',
+    'uiversion': 3
+}
+
+swagger = Swagger(app)
 
 auth = HTTPBasicAuth()
 
